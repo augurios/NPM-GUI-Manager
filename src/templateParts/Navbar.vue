f02bf466b06600fdfc73e41cf68226494fce0d0a
augurios <auguvalerio@gmail.com>
2024-12-23 20:30:46 -0600

    Navbar Clean up

--- src/templateParts/Navbar.vue
+++ src/templateParts/Navbar.vue
@@ -16,7 +16,7 @@
           class="pe-md-3 d-flex align-items-center"
           :class="this.$store.state.isRTL ? 'me-md-auto' : 'ms-md-auto'"
         >
-          <div class="input-group">
+          <!-- <div class="input-group">
             <span class="input-group-text text-body"
               ><i class="fas fa-search" aria-hidden="true"></i
             ></span>
@@ -27,10 +27,10 @@
                 this.$store.state.isRTL ? 'أكتب هنا...' : 'Type here...'
               "
             />
-          </div>
+          </div> -->
         </div>
         <ul class="navbar-nav justify-content-end">
-          <li class="nav-item d-flex align-items-center">
+          <!-- <li class="nav-item d-flex align-items-center">
             <router-link
               :to="{ name: 'Sign In' }"
               class="px-0 nav-link font-weight-bold"
@@ -45,7 +45,7 @@
               >
               <span v-else class="d-sm-inline d-none">Sign In </span>
             </router-link>
-          </li>
+          </li> -->
           <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
             <a
               href="#"
@@ -60,15 +60,7 @@
               </div>
             </a>
           </li>
-          <li class="px-3 nav-item d-flex align-items-center">
-            <a
-              class="p-0 nav-link"
-              @click="toggleConfigurator"
-              :class="textWhite ? textWhite : 'text-body'"
-            >
-              <i class="cursor-pointer fa fa-cog fixed-plugin-button-nav"></i>
-            </a>
-          </li>
+          
           <li
             class="nav-item dropdown d-flex align-items-center"
             :class="this.$store.state.isRTL ? 'ps-2' : 'pe-2'"
@@ -93,7 +85,8 @@
               aria-labelledby="dropdownMenuButton"
             >
               <li class="mb-2">
-                <a class="dropdown-item border-radius-md" href="javascript:;">
+                No New Updates!
+                <!-- <a class="dropdown-item border-radius-md" href="javascript:;">
                   <div class="py-1 d-flex">
                     <div class="my-auto">
                       <img
@@ -113,9 +106,9 @@
                       </p>
                     </div>
                   </div>
-                </a>
+                </a> -->
               </li>
-              <li class="mb-2">
+              <!-- <li class="mb-2">
                 <a class="dropdown-item border-radius-md" href="javascript:;">
                   <div class="py-1 d-flex">
                     <div class="my-auto">
@@ -192,7 +185,7 @@
                     </div>
                   </div>
                 </a>
-              </li>
+              </li> -->
             </ul>
           </li>
         </ul>
